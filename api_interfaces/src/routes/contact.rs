4d2965c35f986dc8f2a8db39b0926ca0c837f19c
Ignacio Borello <ignacio@icanotes.com>
2023-04-16 17:00:02 +1000

    Add partial Eq to ContactBody struct

--- api_interfaces/src/routes/contact.rs
+++ api_interfaces/src/routes/contact.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct ContactBody {
     pub cf_turnstile_token: String,
     pub email: String,
@@ -9,7 +9,17 @@ pub struct ContactBody {
     pub message: String,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct ContactResponse {
     pub message: String
+}
+
+impl PartialEq for ContactBody {
+    fn eq(&self, other: &Self) -> bool {
+        self.email == other.email
+        && self.name == other.name
+        && self.phone == other.phone
+        && self.message == other.message
+        && self.cf_turnstile_token == other.cf_turnstile_token
+    }
 }
